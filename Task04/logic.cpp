ed586dcc708c5d995c1cb9d45508e8eca0990c43
kogicko240 <dmn120112@gmail.com>
2026-05-11 17:34:13 +0300

    17:34 Task04 Done

--- Task04/logic.cpp
+++ Task04/logic.cpp
@@ -9,5 +9,44 @@
 #include "logic.h"
 
 int sum_elements_of_columns_with_extreme_value(int** matrix, int n, int m) {
-	return 0;
+	if (matrix == nullptr || n <= 0 || m <= 0) {
+		return -1;
+	}
+
+	int min_val = matrix[0][0];
+	int max_val = matrix[0][0];
+
+	for (int i = 0; i < n; i++) {
+		if (matrix[i] == nullptr) {
+			return -1;
+		}
+
+		for (int j = 0; j < m; j++) {
+			if (matrix[i][j] < min_val) {
+				min_val = matrix[i][j];
+			}
+			if (matrix[i][j] > max_val) {
+				max_val = matrix[i][j];
+			}
+		}
+	}
+	bool* need_column = new bool[m]();
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (matrix[i][j] == min_val || matrix[i][j] == max_val) {
+				need_column[j] = true;
+			}
+		}
+	}
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (need_column[j]) {
+				sum += matrix[i][j];
+			}
+		}
+	}
+	delete[] need_column;
+	return sum;
 }
